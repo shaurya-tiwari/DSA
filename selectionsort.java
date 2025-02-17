b3c5581388bfa573c26da49496ede9f7a3d216ac
shaurya <shauryatiwari120@gmail.com>
2025-02-18 00:26:10 +0530

    selection Sort

---
+++ selectionsort.java
@@ -0,0 +1,25 @@
+public class selectionsort {
+    public static void main(String[] args) {
+        int array[] = { 3, 6, 8, 9, 5, 2, 6, 9, 4, 1, 5 };
+        selectionSort(array);
+        for (int arr : array) {
+            System.out.print(arr + " ");
+        }
+
+    }
+
+    // we take nestedloop when we neeed to compare two itrations
+    public static void selectionSort(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            int minumum = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minumum] > array[j]) {
+                    minumum = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[minumum];
+            array[minumum] = temp;
+        }
+    }
+}
