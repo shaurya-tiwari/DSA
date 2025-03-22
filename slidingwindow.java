e0c36e11ad43a3ba08215d8bee65743ec3b21836
shaurya <shauryatiwari120@gmail.com>
2025-03-22 22:55:58 +0530

    Easy

---
+++ slidingwindow.java
@@ -0,0 +1,58 @@
+//  maintain a "window" (a subset of data) that slides across the data structure to efficiently process it, 
+// often reducing the need for nested loops and improving time complexity. 
+// time coplexity = 
+
+
+import java.io.*;
+import java.util.*;
+
+class CHOCOLATEDISTRIBUTION {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                arr.add(Integer.parseInt(token));
+            }
+
+            int m = Integer.parseInt(br.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.findMinDiff(arr, m));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+// ______________________________________________________________________________________________
+// User function Template for Java
+class Solution {
+    public int findMinDiff(ArrayList<Integer> arr, int m) {
+
+        Collections.sort(arr);
+        int mindiff = Integer.MAX_VALUE;
+        for (int i = 0; i + m - 1 < arr.size(); i++) {
+            // calculate the diffrence of current window
+            int diff = arr.get(i + m - 1) - arr.get(i); // means minimmumelement - maximum element in itrated numb
+            if (diff < mindiff) {
+                mindiff = diff;
+            }
+
+        }
+        return mindiff;
+    }
+
+    public static void main(String[] args) {
+        Solution ob = new Solution();
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(3, 4, 1, 9, 56, 7, 9, 12));
+        System.out.println(ob.findMinDiff(arr, 5));
+    }
+}
