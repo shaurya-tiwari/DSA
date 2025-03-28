a6572c7a94b8858ac9538dced24e725d9b57644e
shaurya <shauryatiwari120@gmail.com>
2025-03-28 23:43:51 +0530

    theory

---
+++ SpaceandTimeCoplexity.java
@@ -0,0 +1,29 @@
+// TIME COMPLEXITY - Relation between input size and running time operation .
+
+// BEST CASE-omega(1) 
+    // Best case time complexity is O(1) when the algorithm performs a constant amount of time regardless of the input size.
+    // Examples:
+    //  - Accessing an element in an array by its index
+    //  - Finding the maximum or minimum element in an array
+    //  - Checking if a value exists in a sorted array
+
+// AVERAGE CASE- theta(n+1/2)
+    // Average case time complexity is O(n) when the algorithm performs a linear amount of time, where n is the input size.
+    // Examples:
+    //  - Finding the sum of elements in an array
+    //  - Sorting an array
+    //  - Searching for an element in an unsorted array
+
+
+// if the loop is nested the n will be multiply(nxn) and when there is the two diffrent loops are there the the n+n will be consider
+
+// SPACE COMPLEXITY  - how many variables are in the code 
+    // Space complexity is the amount of memory an algorithm uses, not including the input size.
+    // Examples:
+    //  - Space complexity of a simple search algorithm is O(1)
+    //  - Space complexity of a recursive algorithm is O(n)
+
+
+ // in worst case the space complexity will be O(n) because in worst case the recursive function call stack will be of n size.
+ // in average case the space complexity will be O(1) because in average case the recursive function call stack will be of constant size.
+
