bf7c93808a7c8bc5841e78b93e6cdddd4321846f
shaurya <shauryatiwari120@gmail.com>
2025-03-30 23:52:50 +0530

    linear search

--- LinearSearchhh.java
+++ LinearSearchhh.java
@@ -14,7 +14,7 @@ public class LinearSearchhh {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == value) {
                 return i;
-
+                 
             }
         }
         return -1;
