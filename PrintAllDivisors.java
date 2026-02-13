11c9604b0a2b1fdec9f18d816a4415a1b36d4ba6
shaurya <shauryatiwari120@gmail.com>
2026-02-13 23:20:11 +0530

    nuu

--- PrintAllDivisors.java
+++ PrintAllDivisors.java
@@ -1,9 +1,13 @@
 public class PrintAllDivisors {
 
     public void printnumbers(int num) {
-        for (int i = 1; i <= num; i++) { // not include 0 becouse  its infinite 
+        for (int i = 1; i <= Math.sqrt(num); i++) { // not include 0 becouse its infinite
             if (num % i == 0) {
                 System.out.println(i);
+
+                if (i != num / i) {
+                    System.out.println(num / i);
+                }
             }
         }
     }
