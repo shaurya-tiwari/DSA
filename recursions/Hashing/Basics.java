5e274563d445071d7f4719970db9608e7c4ba8eb
shaurya <shauryatiwari120@gmail.com>
2026-02-18 00:18:51 +0530

    Enhance comments and clarify frequency count logic in Basics.java

--- recursions/Hashing/Basics.java
+++ recursions/Hashing/Basics.java
@@ -1,6 +1,34 @@
 // package Hashing;
-// Hashing = 
+// Hashing = pre-storing and fething when required ,
+// hashmap ,key must be uique
+//  hashset ,= no dublicate allow  , only sinle items, no K,V 
+// hashtable , = key  must be unique , no null values 
+//  linkedHash=
+
+import java.util.*;;
 
 public class Basics {
-    
+    // we need to ensure that the big number in array shoudl be in comes in the
+    // index
+    public void frequencycount() {
+
+        int[] arr = { 2, 3, 4, 6, 6, 7, 3, 5, 9, 3, 8, 3, 10, 12 };
+        // means the hashset will be the size of 13
+        int[] hasharr = new int[14];
+
+        for (int i = 0; i < arr.length; i++) {
+            // make the simple element in hash array
+            int number = arr[i];
+            hasharr[number] = hasharr[number] + 1; // means ,index of hasharray is 5 , but the value is initially =0 but
+            // after checking that 5 , the value becames 0 to 1 , means the 5 number
+            // is counted as 1 , then next 1+++++++ e
+        }
+        System.out.println("frquency of number is " + hasharr[3]);
+    }
+
+    public static void main(String[] args) {
+        Basics Basics = new Basics();
+        Basics.frequencycount();
+
+    }
 }
