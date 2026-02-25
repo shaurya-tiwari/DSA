c122b1bd489d3ca743a42c8ae7256b0bf27278d5
shaurya <shauryatiwari120@gmail.com>
2026-02-26 00:53:38 +0530

    new

---
+++ ARRAY/secondLargestelem.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class secondLargestelem {
+    public void findseceelem(int[] arr, int n) {
+
+    }
+
+}
+
+public static void main(String[] args) {
+    secondLargestelem secondLargestelem = new secondLargestelem();
+    int[] arr = { 22, 11, 45, 77, 44, 26, 87, 98, 105 };
+
+    secondLargestelem.findseceelem(arr, arr.length);
+}
+
+// Arrays.sort(arr);
+// return arr[n - 2];
+// }
