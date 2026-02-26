bcaca2110b0f615e271bee09b5086f901edfc8c0
shaurya <shauryatiwari120@gmail.com>
2026-02-27 00:56:08 +0530

    new

--- ARRAY/checkArrayisSortedorNot.java
+++ ARRAY/checkArrayisSortedorNot.java
@@ -1,7 +1,7 @@
 public class checkArrayisSortedorNot {
     public boolean check(int[] arr, int n) {
 
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n-1; i++) { // itrate till second last or check to next which the last 
             if (arr[i] > arr[i + 1]) {
                 return false;
             }
