0a45f899e265f480f868ae58399da794970ff570
shaurya <shauryatiwari120@gmail.com>
2026-02-28 00:15:09 +0530

    done

--- ARRAY/checkArrayisSortedorNot.java
+++ ARRAY/checkArrayisSortedorNot.java
@@ -1,10 +1,12 @@
 public class checkArrayisSortedorNot {
-    public boolean check(int[] arr, int n) {
-
-        for (int i = 0; i < n-1; i++) { // itrate till second last or check to next which the last 
-            if (arr[i] > arr[i + 1]) {
-                return false;
+    public boolean check(int[] nums, int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) { // itrate till second last or check to next which the last
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
             }
+            if (count > 1)
+                return false;
         }
 
         return true;
@@ -12,8 +14,8 @@ public class checkArrayisSortedorNot {
 
     public static void main(String[] args) {
         checkArrayisSortedorNot checkArrayisSortedorNot = new checkArrayisSortedorNot();
-        int[] arr = { 1, 2, 3, 4, 5, 8, 9 };
-        int[] arr2 = { 6, 2, 3, 7, 5, 8, 9 };
-        System.out.println(checkArrayisSortedorNot.check(arr2, arr2.length));
+        int[] nums = { 1, 2, 3, 4, 5, 8, 9 };
+        int[] nums2 = { 2, 1, 3, 4 };
+        System.out.println(checkArrayisSortedorNot.check(nums2, nums2.length));
     }
 }
