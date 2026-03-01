69d616452291df413249ca8c948304257dde63b8
shaurya <shauryatiwari120@gmail.com>
2026-03-01 23:30:10 +0530

    new

---
+++ ARRAY/rotateArrayByK.java
@@ -0,0 +1,17 @@
+public class rotateArrayByK {
+
+    public void rotate(int[] nums, int k) {
+        
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        rotateArrayByK rotateArrayByK = new rotateArrayByK();
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+
+    }
+}
